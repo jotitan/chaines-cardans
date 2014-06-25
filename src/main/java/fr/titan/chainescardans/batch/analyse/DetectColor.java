09ca72f4160ac455751ae8d733da38d7c03d1dca
jotitan <titanbar@hotmail.com>
2014-06-25 17:55:50 +0200

    Ajout algo

--- src/main/java/fr/titan/chainescardans/batch/analyse/DetectColor.java
+++ src/main/java/fr/titan/chainescardans/batch/analyse/DetectColor.java
@@ -5,24 +5,80 @@ import java.awt.image.BufferedImage;
 import java.awt.image.Raster;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
 
 /**
  *
  */
 public class DetectColor {
 
-    public String detectFirstColor(String path) throws Exception {
+    public int[][][] detectFirstColor(String path) throws Exception {
         File file = new File(path);
         BufferedImage img = ImageIO.read(file);
         Raster data = img.getData();
 
-        int[] global = detectColor(data, false);
-        int[] center = detectColor(data, true);
+        int[][] global = detectColor(data, false);
+        int[][] center = detectColor(data, true);
 
-        return null;
+        return new int[][][] { global, center };
     }
 
-    private int[] detectColor(Raster data, boolean center) {
+    class KeyColor {
+        private int red;
+        private int green;
+        private int blue;
+
+        public KeyColor(int red, int green, int blue) {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+        }
+
+        public KeyColor(int red, int green, int blue, int round) {
+            this.red = roundTo(red, round);
+            this.green = roundTo(green, round);
+            this.blue = roundTo(blue, round);
+        }
+
+        public int[] toColor() {
+            return new int[] { red, green, blue };
+        }
+
+        public boolean isGray() {
+            return red == green && green == blue;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o)
+                return true;
+            if (o == null || getClass() != o.getClass())
+                return false;
+
+            KeyColor keyColor = (KeyColor) o;
+
+            if (blue != keyColor.blue)
+                return false;
+            if (green != keyColor.green)
+                return false;
+            if (red != keyColor.red)
+                return false;
+
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = red;
+            result = 31 * result + green;
+            result = 31 * result + blue;
+            return result;
+        }
+    }
+
+    private int[][] detectColor(Raster data, boolean center) {
         double[][] results = new double[3][256];
 
         int beginX = center ? data.getWidth() / 4 : 0;
@@ -32,8 +88,15 @@ public class DetectColor {
 
         int totalPoints = (endX - beginX) * (endY - beginY);
 
+        HashMap<KeyColor, Integer> countByColor = new HashMap<KeyColor, Integer>();
         for (int x = beginX; x < endX; x++) {
             for (int y = beginY; y < endY; y++) {
+                KeyColor key = new KeyColor(data.getSample(x, y, 0), data.getSample(x, y, 1), data.getSample(x, y, 2), 51);
+                if (!countByColor.containsKey(key)) {
+                    countByColor.put(key, 1);
+                } else {
+                    countByColor.put(key, countByColor.get(key) + 1);
+                }
                 for (int color = 0; color < 3; color++) {
                     results[color][data.getSample(x, y, color)]++;
                     // results[color][data.getSample(x, y, color) / 5]++;
@@ -41,35 +104,63 @@ public class DetectColor {
             }
         }
 
-        int[] moyennes = new int[3];
         int tempTotal[] = new int[3];
-        int medianes[] = new int[3];
         int tempMax[] = new int[3];
+        double[] moyennes = new double[3];
+        int medianes[] = new int[3];
         int max[] = new int[3];
+        // Regroupe par couleur complete
         // Tester avec l'element le plus present. Regrouper tous les 5 pour lisser les resultats
-        for (int x = 0; x < 256; x++) {
+        for (int x = 00; x < 256; x++) {
             for (int color = 0; color < 3; color++) {
+                // Resultats > 1%
+                double value = results[color][x];
                 if (tempTotal[color] < totalPoints / 2) {
-                    tempTotal[color] += results[color][x];
+                    tempTotal[color] += value;
                 } else {
                     if (medianes[color] == 0) {
                         medianes[color] = x;
                     }
                 }
-                moyennes[color] += (results[color][x] / totalPoints) * x;
-                if (tempMax[color] < results[color][x]) {
-                    tempMax[color] = (int) results[color][x];
-                    max[color] = x;
+                if (value / totalPoints >= 0.01) {
+                    moyennes[color] += (value / totalPoints) * x;
+                    if (tempMax[color] < value) {
+                        tempMax[color] = (int) value;
+                        max[color] = x;
+                    }
+                }
+            }
+        }
+
+        // Calcul max par couleur
+        int maxValue = 0;
+        KeyColor maxKey = null;
+        for (KeyColor key : countByColor.keySet()) {
+            /* 3 cas : plus grand et non gris, gris et 3 fois plus, 3 fois moins mais non gris */
+            if ((countByColor.get(key) > maxValue && !key.isGray())) {// || //
+                // (key.isGray() && maxValue / countByColor.get(key) < 0.3) || //
+                // (!key.isGray() && maxValue / countByColor.get(key) > 0.3)) {
+                // On peut ajouter un test pour privilegier les couches differentes (pour eviter le gris), meme si un peu inf
+                if (!key.isGray() || maxValue / countByColor.get(key) < 0.3) {
+                    maxValue = countByColor.get(key);
+                    maxKey = key;
                 }
             }
         }
 
         // On arrondi
-        for (int color = 0; color < 3; color++) {
-            moyennes[color] = (moyennes[color] / 51) * 51 + ((moyennes[color] % 51 > 25) ? 51 : 0);
-            medianes[color] = (medianes[color] / 51) * 51 + ((medianes[color] % 51 > 25) ? 51 : 0);
-        }
-        return medianes;
+        // for (int color = 0; color < 3; color++) {
+        // moyennes[color] = (moyennes[color] / 51) * 51 + ((moyennes[color] % 51 > 25) ? 51 : 0);
+        // medianes[color] = (medianes[color] / 51) * 51 + ((medianes[color] % 51 > 25) ? 51 : 0);
+        // }
+        int[] average = new int[] { (int) moyennes[0], (int) moyennes[1], (int) moyennes[2] };
+
+        return new int[][] { average, medianes, max, maxKey.toColor() };
+    }
+
+    // Arrondi au a la valeur superieur
+    private int roundTo(int value, int pas) {
+        return value % pas == 0 ? value : value + (pas - value % pas);
     }
 
     public String convertColorFromHexa(String color) {
--- src/test/java/fr/titan/chainescardans/batch/analyse/DetectColorTest.java
+++ src/test/java/fr/titan/chainescardans/batch/analyse/DetectColorTest.java
@@ -3,17 +3,81 @@ package fr.titan.chainescardans.batch.analyse;
 import junit.framework.Assert;
 import junit.framework.TestCase;
 
+import java.io.File;
+import java.io.FileOutputStream;
+
 /**
  * Created with IntelliJ IDEA. User: 960963 Date: 24/06/14 Time: 10:06 To change this template use File | Settings | File Templates.
  */
 public class DetectColorTest extends TestCase {
+    private DetectColor detectColor = new DetectColor();
 
     public void testDetectFirstColor() throws Exception {
-        DetectColor detectColor = new DetectColor();
-        detectColor.detectFirstColor(getClass().getResource("/test-perso.jpg").getPath());
-        detectColor.detectFirstColor(getClass().getResource("/test_bleu.jpg").getPath());
-        detectColor.detectFirstColor(getClass().getResource("/test-moto.jpg").getPath());
 
+        File file = File.createTempFile("test-result", ".html");
+        FileOutputStream out = new FileOutputStream(file);
+
+        getColorResults("/test-perso.jpg", out);
+        getColorResults("/test_bleu.jpg", out);
+        getColorResults("/test-moto.jpg", out);
+        getColorResults("/test-moto-rouge.jpg", out);
+        getColorResults("/test-moto-blue.jpg", out);
+        getColorResults("/pull-rouge.jpg", out);
+
+        out.close();
+        System.out.println(file.getAbsolutePath());
         // Assert.assertEquals("blue", color);
+
+    }
+
+    private void getColorResults(String filename, FileOutputStream out) throws Exception {
+        String path = getClass().getResource(filename).getPath();
+        int[][][] infos = detectColor.detectFirstColor(path);
+
+        out.write(("Result " + filename + "<br/>").getBytes());
+        out.write(("<img src='" + path + "'/><br/>").getBytes());
+        String block = "<span style='display:inline-block;width:50px;height:20px;background-color:";
+
+        out.write("Global : ".getBytes());
+        out.write(("AVG : " + block + formatColor(infos[0][0]) + "'></span>").getBytes());
+        out.write(("MED : " + block + formatColor(infos[0][1]) + "'></span>").getBytes());
+        out.write(("MAX : " + block + formatColor(infos[0][2]) + "'></span>").getBytes());
+        out.write(("MAX GROUP : " + block + formatColor(infos[0][3]) + "'></span><br/>").getBytes());
+
+        out.write("Centre : ".getBytes());
+        out.write(("AVG : " + block + formatColor(infos[1][0]) + "'></span>").getBytes());
+        out.write(("MED : " + block + formatColor(infos[1][1]) + "'></span>").getBytes());
+        out.write(("MAX : " + block + formatColor(infos[1][2]) + "'></span>").getBytes());
+        out.write(("MAX GROUP : " + block + formatColor(infos[1][3]) + "'></span><br/><hr/>").getBytes());
+    }
+
+    private String formatColor(int[] color) {
+        return "rgb(" + color[0] + "," + color[1] + "," + color[2] + ")";
+    }
+
+    private String detectColor(int[] color) {
+        if (color[0] > 200 && color[1] > 200 && color[2] < 100) {
+            return "YELLOW";
+        }
+        if (color[0] > 200 && color[1] < 100 && color[2] < 100) {
+            return "RED";
+        }
+        if (color[0] < 100 && color[1] < 100 && color[2] > 200) {
+            return "BLUE";
+        }
+        if (color[0] < 100 && color[1] > 200 && color[2] < 100) {
+            return "GREEN";
+        }
+        if (color[0] > 200 && color[1] < 100 && color[2] > 200) {
+            return "PURPLE";
+        }
+        if (color[0] > 200 && color[1] > 200 && color[2] > 200) {
+            return "WHITE";
+        }
+
+        if (color[0] < 100 && color[1] < 100 && color[2] < 100) {
+            return "BLACK";
+        }
+        return "";
     }
 }
